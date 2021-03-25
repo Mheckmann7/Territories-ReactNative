c3b26466c90aa7313e3283b221aa1dc03e82d954
Mira Heckmann <miraheckmann@gmail.com>
2021-03-24 18:09:01 -0700

    adds map and coords

---
+++ client/pages/Map.js
@@ -0,0 +1,51 @@
+
+
+import React, { useState, useEffect } from "react";
+import {StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
+// import * as Permissions from 'expo-permissions';
+
+export default function Map(props) {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+          let { status } = await Location.requestPermissionsAsync();
+          if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+          }
+    
+          let location = await Location.getCurrentPositionAsync({});
+          setLocation(location);
+        })();
+      }, []);
+    
+      let text = 'Waiting..';
+      if (errorMsg) {
+        text = errorMsg;
+      } else if (location) {
+        text = JSON.stringify(location);
+      }
+    
+      return (
+        <View>
+              <Text>{text}</Text>
+              <MapView style={styles.map} />
+        </View>
+      );
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  });
