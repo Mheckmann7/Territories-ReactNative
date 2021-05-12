c6d8daa9ffbd896b8a2035c9fbc1e979be8e4073
Mira Heckmann <miraheckmann@gmail.com>
2021-05-12 18:05:44 -0400

    adds home page

---
+++ components/images.js
@@ -0,0 +1,10 @@
+export default [{
+    image: require('../assets/home.jpg')
+    },
+    {
+    image: require('../assets/home1.jpg')
+    },
+    {
+    image: require('../assets/home2.jpg')
+    }
+]
---
+++ components/imgItems.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, ImageBackground, StyleSheet, Image, Dimensions } from 'react-native'
+
+import img from '../assets/home.jpg';
+import img2 from '../assets/home1.jpg';
+import img3 from '../assets/home2.jpg';
+const { width} = Dimensions.get("window");
+const height = '100%'
+
+// const images = [img, img2, img3]
+
+const ImgItem = (props) => {
+    const { image } = props.image;
+    return (
+        <View>
+                <Image
+                 
+                    source={image}
+                    style={styles.image}
+                />
+        </View>
+    )
+}
+
+export default ImgItem
+
+const styles = StyleSheet.create({
+    image: {
+        width,
+        height,
+        resizeMode: 'cover',
+    },
+})
